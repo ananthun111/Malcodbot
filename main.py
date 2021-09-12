f8b67366ae5cba4a7e413f4577e2182a37dc145c
ananthu <54952618+ananthun111@users.noreply.github.com>
2021-09-12 14:40:19 +0000

    aaa

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import os
 import discord
+import csv
+import random
+
+
+phrases = []
+with open("phrases.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        phrases.append(row[1])
+
 
 bot = discord.Client()
 @bot.event
@@ -10,6 +20,16 @@ async def on_ready():
 async def on_message(msg):
   if msg.content.startswith("!ping"):
     await msg.channel.send("pong")
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith("$greet"):
+        await message.channel.send(f"Hello! How are you {message.author}")
+    elif "$thought" in message.content:
+        response = phrases[random.randint(0, len(phrases) - 1)]
+        await message.channel.send(response)
+
+
 
 
 
